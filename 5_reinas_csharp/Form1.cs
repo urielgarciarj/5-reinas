3df9b7b957c78b48da5788ab277ba4caacc23360
Ngakk <ngakwastaken@gmail.com>
2019-11-09 19:30:52 -0600

    backtracking done

--- 5_reinas_csharp/Form1.cs
+++ 5_reinas_csharp/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -11,9 +13,14 @@ using System.Windows.Forms;
 
 namespace _5_reinas_csharp
 {
+    public class Pos
+    {
+        public int x, y;
+    }
+
     public partial class Form1 : Form
     {
-        static Image reinaImg = Image.FromFile("C:/Users/Uriel/Pictures/coronachida.PNG");
+        static Image reinaImg;
 
         static PictureBox[,] boxes;
 
@@ -27,11 +34,14 @@ namespace _5_reinas_csharp
                 { pictureBox16, pictureBox17, pictureBox18, pictureBox19, pictureBox20},
                 { pictureBox21, pictureBox22,pictureBox23, pictureBox24, pictureBox25}
             };
+            string absolutePath = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), @"coronachida.png");
+            reinaImg = Image.FromFile(absolutePath);
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            elmein();
+            //elmein();
+            backtrack();
         }
 
         private void Ajedrez_Load(object sender, EventArgs e)
@@ -41,8 +51,8 @@ namespace _5_reinas_csharp
 
         public static class GlobalData
         {
-            public static int[] reinas = new int[5];
             public static int n = 5;
+            public static int[] reinas = new int[n];
         }
 
         static async Task colocarReina(int i, bool solucion)
@@ -90,6 +100,24 @@ namespace _5_reinas_csharp
                 }
             }
             return v;
+        }
+
+        static bool esPosible2(int[,] tablero, int x, int y)
+        {
+            for (int i = 0; i < GlobalData.n; i++)
+            {
+                for(int j = 0; j < GlobalData.n; j++)
+                {
+                    if(tablero[i,j] == 1)
+                    {
+                        if(i == x || j == y || (Math.Abs(i-x) == Math.Abs(y-j)))
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
         }
 
         static bool estaSolucionado()
@@ -169,6 +197,29 @@ namespace _5_reinas_csharp
             
         }
 
+        static void mostrarAjedrez2(int[,] tablero)
+        {
+            //MOSTRAR TABLERO
+            //Console.WriteLine("MOSTRANDO TABLERO");
+            for (int i = 0; i < GlobalData.n; i++)
+            {
+                for (int j = 0; j < GlobalData.n; j++)
+                {
+                    if (tablero[i, j] == 1)
+                    {
+                        //poner reina
+                        boxes[i, j].Image = reinaImg;
+                    }
+                    else
+                    {
+                        //Quitar reina
+                        boxes[i, j].Image = null;
+                    }
+                }
+            };
+
+        }
+
         static async void elmein()
         {
             bool flag = true;
@@ -182,6 +233,99 @@ namespace _5_reinas_csharp
 
             mostrarAjedrez();
             MessageBox.Show("¡10 SOLUCIONES ENCONTRADAS!", "Finalizado");
+        }
+
+        static async Task<bool> placeQueen(int[,] tablero, int _n)
+        {
+            if (_n >= GlobalData.n)
+            {
+                MessageBox.Show("Solucion encontrada");
+                return true;
+            }
+
+            bool ramaErronea = true;
+
+            for (int i = 0; i < GlobalData.n; i++)
+            {
+                if(esPosible2(tablero, i, _n))
+                {
+                    tablero[i, _n] = 1;
+
+                    mostrarAjedrez2(tablero);
+                    await Task.Delay(100);
+
+                    if (await placeQueen(tablero, _n + 1))
+                    {
+                        ramaErronea = false;
+                        //return true;
+                    }
+
+                    tablero[i, _n] = 0;
+                }
+
+                /*for (int j = 0; j < GlobalData.n; j++)
+                {
+                    if(tablero[i,j] == 0) //esta esta celda vacia?
+                    {
+                        if(esPosible2(tablero, i,j)) //ninguna reina amenaza esta celda?
+                        {
+                            tablero[i, j] = 1;
+
+                            mostrarAjedrez2(tablero);
+
+                            await Task.Delay(100);
+
+                            await placeQueen(tablero, _n + 1); 
+                        }
+                    }
+                }*/
+            }
+
+            //Si llega aquí es una rama erronea
+            //MessageBox.Show("Rama erronea");
+            if(!ramaErronea)
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("Rama mala");
+                return false;
+            }
+        }
+
+        static async void backtrack()
+        {
+            int[,] tablero = new int[GlobalData.n, GlobalData.n];
+
+            for (int i = 0; i < GlobalData.n; i++)
+            {
+                for (int j = 0; j < GlobalData.n; j++)
+                {
+                    tablero[i, j] = 0;
+                }
+            }
+            
+            mostrarAjedrez2(tablero);
+
+            await placeQueen(tablero, 0);
+            /*
+            for (int t = 0; t < GlobalData.n; t++)
+            {
+                for (int i = 0; i < GlobalData.n; i++)
+                {
+                    for (int j = 0; j < GlobalData.n; j++)
+                    {
+                        tablero[i, j] = 0;
+                    }
+                }
+
+                tablero[t, 0] = 1;
+
+                mostrarAjedrez2(tablero);
+
+                await placeQueen(tablero, 1);
+            }*/
         }
     }
 }
